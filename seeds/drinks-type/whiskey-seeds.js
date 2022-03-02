c49db05484f05ed260d234c937eb876fa32b2207
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-02 13:53:02 -0600

    Added Whiskey seeds

---
+++ seeds/drinks-type/whiskey-seeds.js
@@ -0,0 +1,22 @@
+const { Drink } = require("../models");
+
+const whiskeyDrinksData = [
+  {
+    name: "Damned if you do",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/ql7bmx1503565106.jpg",
+  },
+  {
+    name: "Hot Toddy",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/ggx0lv1613942306.jpg",
+  },
+  {
+    name: "Owen's Grandmother's Revenge",
+    image:
+      "https://www.thecocktaildb.com/images/media/drink/0wt4uo1503565321.jpg",
+  },
+];
+const seedWhiskeyDrinks = () => Drink.bulkCreate(whiskeyDrinksData);
+
+module.exports = seedWhiskeyDrinks;
