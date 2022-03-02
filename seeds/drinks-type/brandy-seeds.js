b9f99f171d7b30f7e1ff052c421562100ab6cea9
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-02 14:04:40 -0600

    Changed double quotes to single quotes in seeds files

--- seeds/drinks-type/brandy-seeds.js
+++ seeds/drinks-type/brandy-seeds.js
@@ -3,92 +3,92 @@ const { Drink } = require('../models');
 const brandyDrinksData = [
 
     {
-        name: "Amaretto Shake",
-        image: "https://www.thecocktaildb.com/images/media/drink/xk79al1493069655.jpg"
+        name: 'Amaretto Shake',
+        image: 'https://www.thecocktaildb.com/images/media/drink/xk79al1493069655.jpg'
     },
     {
-        name: "Bermuda Highball",
-        image: "https://www.thecocktaildb.com/images/media/drink/qrvtww1441206528.jpg"
+        name: 'Bermuda Highball',
+        image: 'https://www.thecocktaildb.com/images/media/drink/qrvtww1441206528.jpg'
     },
     {
-        name: "Between The Sheets",
-        image: "https://www.thecocktaildb.com/images/media/drink/of1rj41504348346.jpg"
+        name: 'Between The Sheets',
+        image: 'https://www.thecocktaildb.com/images/media/drink/of1rj41504348346.jpg'
     },
     {
-        name: "Boston Sidecar",
-        image: "https://www.thecocktaildb.com/images/media/drink/qzs5d11504365962.jpg"
+        name: 'Boston Sidecar',
+        image: 'https://www.thecocktaildb.com/images/media/drink/qzs5d11504365962.jpg'
     },
     {
-        name: "Brandy Alexander",
-        image: "https://www.thecocktaildb.com/images/media/drink/mlyk1i1606772340.jpg"
+        name: 'Brandy Alexander',
+        image: 'https://www.thecocktaildb.com/images/media/drink/mlyk1i1606772340.jpg'
     },
     {
-        name: "Brandy Flip",
-        image: "https://www.thecocktaildb.com/images/media/drink/6ty09d1504366461.jpg"
+        name: 'Brandy Flip',
+        image: 'https://www.thecocktaildb.com/images/media/drink/6ty09d1504366461.jpg'
     },
     {
-        name: "Brandy Sour",
-        image: "https://www.thecocktaildb.com/images/media/drink/b1bxgq1582484872.jpg"
+        name: 'Brandy Sour',
+        image: 'https://www.thecocktaildb.com/images/media/drink/b1bxgq1582484872.jpg'
     },
     {
-        name: "City Slicker",
-        image: "https://www.thecocktaildb.com/images/media/drink/dazdlg1504366949.jpg"
+        name: 'City Slicker',
+        image: 'https://www.thecocktaildb.com/images/media/drink/dazdlg1504366949.jpg'
     },
     {
-        name: "English Highball",
-        image: "https://www.thecocktaildb.com/images/media/drink/dhvr7d1504519752.jpg"
+        name: 'English Highball',
+        image: 'https://www.thecocktaildb.com/images/media/drink/dhvr7d1504519752.jpg'
     },
     {
-        name: "Gentleman's Club",
-        image: "https://www.thecocktaildb.com/images/media/drink/k2r7wv1582481454.jpg"
+        name: 'Gentleman's Club',
+        image: 'https://www.thecocktaildb.com/images/media/drink/k2r7wv1582481454.jpg'
     },
     {
-        name: "Horse's Neck",
-        image: "https://www.thecocktaildb.com/images/media/drink/006k4e1504370092.jpg"
+        name: 'Horse's Neck',
+        image: 'https://www.thecocktaildb.com/images/media/drink/006k4e1504370092.jpg'
     },
     {
-        name: "Kioki Coffee",
-        image: "https://www.thecocktaildb.com/images/media/drink/uppqty1441247374.jpg"
+        name: 'Kioki Coffee',
+        image: 'https://www.thecocktaildb.com/images/media/drink/uppqty1441247374.jpg'
     },
     {
-        name: "Mississippi Planters Punch",
-        image: "https://www.thecocktaildb.com/images/media/drink/urpyqs1439907531.jpg"
+        name: 'Mississippi Planters Punch',
+        image: 'https://www.thecocktaildb.com/images/media/drink/urpyqs1439907531.jpg'
     },
     {
-        name: "Port Wine Cocktail",
-        image: "https://www.thecocktaildb.com/images/media/drink/qruprq1441553976.jpg"
+        name: 'Port Wine Cocktail',
+        image: 'https://www.thecocktaildb.com/images/media/drink/qruprq1441553976.jpg'
     },
     {
-        name: "Porto flip",
-        image: "https://www.thecocktaildb.com/images/media/drink/64x5j41504351518.jpg"
+        name: 'Porto flip',
+        image: 'https://www.thecocktaildb.com/images/media/drink/64x5j41504351518.jpg'
     },
     {
-        name: "Quaker's Cocktail",
-        image: "https://www.thecocktaildb.com/images/media/drink/yrqppx1478962314.jpg"
+        name: 'Quaker's Cocktail',
+        image: 'https://www.thecocktaildb.com/images/media/drink/yrqppx1478962314.jpg'
     },
     {
-        name: "Scooter",
-        image: "https://www.thecocktaildb.com/images/media/drink/twuptu1483388307.jpg"
+        name: 'Scooter',
+        image: 'https://www.thecocktaildb.com/images/media/drink/twuptu1483388307.jpg'
     },
     {
-        name: "Scotch Cobbler",
-        image: "https://www.thecocktaildb.com/images/media/drink/1q7coh1504736227.jpg"
+        name: 'Scotch Cobbler',
+        image: 'https://www.thecocktaildb.com/images/media/drink/1q7coh1504736227.jpg'
     },
     {
-        name: "Sidecar Cocktail",
-        image: "https://www.thecocktaildb.com/images/media/drink/ewjxui1504820428.jpg"
+        name: 'Sidecar Cocktail',
+        image: 'https://www.thecocktaildb.com/images/media/drink/ewjxui1504820428.jpg'
     },
     {
-        name: "Sol Y Sombra",
-        image: "https://www.thecocktaildb.com/images/media/drink/3gz2vw1503425983.jpg"
+        name: 'Sol Y Sombra',
+        image: 'https://www.thecocktaildb.com/images/media/drink/3gz2vw1503425983.jpg'
     },
     {
-        name: "Stinger",
-        image: "https://www.thecocktaildb.com/images/media/drink/2ahv791504352433.jpg"
+        name: 'Stinger',
+        image: 'https://www.thecocktaildb.com/images/media/drink/2ahv791504352433.jpg'
     },
     {
-        name: "Victor",
-        image: "https://www.thecocktaildb.com/images/media/drink/voapgc1492976416.jpg"
+        name: 'Victor',
+        image: 'https://www.thecocktaildb.com/images/media/drink/voapgc1492976416.jpg'
     }
 
 ];
--- seeds/drinks-type/tequila-seeds.js
+++ seeds/drinks-type/tequila-seeds.js
@@ -1,140 +1,140 @@
-const { Drink } = require("../models");
+const { Drink } = require('../models');
 
 const tequilaDrinksData = [
   {
-    name: "110 in the shade",
+    name: '110 in the shade',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/xxyywq1454511117.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/xxyywq1454511117.jpg'
   },
   {
-    name: "3-Mile Long Island Iced Tea",
+    name: '3-Mile Long Island Iced Tea',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg'
   },
   {
-    name: "Adam Bomb",
+    name: 'Adam Bomb',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/tpxurs1454513016.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/tpxurs1454513016.jpg'
   },
   {
-    name: "Amaretto Stone Sour Alternative",
+    name: 'Amaretto Stone Sour Alternative',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/wutxqr1472720012.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/wutxqr1472720012.jpg'
   },
   {
-    name: "Apple Grande",
+    name: 'Apple Grande',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/wqrptx1472668622.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/wqrptx1472668622.jpg'
   },
   {
-    name: "Bloody Maria",
+    name: 'Bloody Maria',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/yz0j6z1504389461.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/yz0j6z1504389461.jpg'
   },
   {
-    name: "Blue Margarita",
+    name: 'Blue Margarita',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/bry4qh1582751040.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/bry4qh1582751040.jpg'
   },
   {
-    name: "Brave Bull Shooter",
+    name: 'Brave Bull Shooter',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/yrtypx1473344625.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/yrtypx1473344625.jpg'
   },
   {
-    name: "Cherry Electric Lemonade",
+    name: 'Cherry Electric Lemonade',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/tquyyt1451299548.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/tquyyt1451299548.jpg'
   },
   {
-    name: "Downshift",
+    name: 'Downshift',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/y36z8c1503563911.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/y36z8c1503563911.jpg'
   },
   {
-    name: "Long Island Iced Tea",
+    name: 'Long Island Iced Tea',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/wx7hsg1504370510.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/wx7hsg1504370510.jpg'
   },
   {
-    name: "Margarita",
+    name: 'Margarita',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg'
   },
   {
-    name: "Moranguito",
+    name: 'Moranguito',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/urpsyq1475667335.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/urpsyq1475667335.jpg'
   },
   {
-    name: "Paloma",
+    name: 'Paloma',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/samm5j1513706393.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/samm5j1513706393.jpg'
   },
   {
-    name: "Pineapple Paloma",
+    name: 'Pineapple Paloma',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/pg8iw31593351601.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/pg8iw31593351601.jpg'
   },
   {
-    name: "Radioactive Long Island Iced Tea",
+    name: 'Radioactive Long Island Iced Tea',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/rdvqmh1503563512.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/rdvqmh1503563512.jpg'
   },
   {
-    name: "Strawberry Margarita",
+    name: 'Strawberry Margarita',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/tqyrpw1439905311.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/tqyrpw1439905311.jpg'
   },
   {
-    name: "Tequila Fizz",
+    name: 'Tequila Fizz',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/2bcase1504889637.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/2bcase1504889637.jpg'
   },
   {
-    name: "Tequila Slammer",
+    name: 'Tequila Slammer',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/43uhr51551451311.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/43uhr51551451311.jpg'
   },
   {
-    name: "Tequila Sour",
+    name: 'Tequila Sour',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/ek0mlq1504820601.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/ek0mlq1504820601.jpg'
   },
   {
-    name: "Tequila Sunrise",
+    name: 'Tequila Sunrise',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/quqyqp1480879103.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/quqyqp1480879103.jpg'
   },
   {
-    name: "Tequila Surprise",
+    name: 'Tequila Surprise',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/8189p51504735581.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/8189p51504735581.jpg'
   },
   {
-    name: "Tommy's Margarita",
+    name: 'Tommy's Margarita',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/loezxn1504373874.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/loezxn1504373874.jpg'
   },
   {
-    name: "Vampiro",
+    name: 'Vampiro',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/yfhn371504374246.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/yfhn371504374246.jpg'
   },
   {
-    name: "Whitecap Margarita",
+    name: 'Whitecap Margarita',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/srpxxp1441209622.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/srpxxp1441209622.jpg'
   },
   {
-    name: "Winter Paloma",
+    name: 'Winter Paloma',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/u5f0pz1614007748.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/u5f0pz1614007748.jpg'
   },
   {
-    name: "Winter Rita",
+    name: 'Winter Rita',
     image:
-      "https://www.thecocktaildb.com/images/media/drink/fwpd0v1614006733.jpg"
+      'https://www.thecocktaildb.com/images/media/drink/fwpd0v1614006733.jpg'
   },
 ];
 const seedTequilaDrinks = () => Drink.bulkCreate(tequilaDrinksData);
