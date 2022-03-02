c1f51cc045855300784bebae234c23598c4a10b0
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-02 12:13:15 -0600

    created views/ directory, layouts/ and partials/ and a placeholder for main.handlebars. Created User.js inside models which still needs to be defined.

--- main/css/js/register.js
+++ main/css/js/register.js
@@ -1,11 +1,14 @@
 async function signupFormHandler(event) {
     event.preventDefault();
   
+    // need to create login page under views; also need to create model: User
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && email && password) {
+
+    // need to create /api post route
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
---
+++ models/User.js
@@ -0,0 +1,1 @@
+// to create and then store user data in database
