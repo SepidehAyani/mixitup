e28db25195075ff8195128b09933950f8cbae16c
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-03 13:14:25 -0600

    Created possible fields for userDrinksData[{}] in user-seeds.js

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -5,10 +5,40 @@
 // // const { User } = require('../models');
 
 const userDrinksData = [
-
-    {
-
-    }
+  {
+    // user_id,
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    // drink_id
+    drink_name: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'drink',
+        key: 'id',
+      },
+    },
+    // user_name,
+    user_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'user',
+        key: 'name',
+      },
+    },
+    // drink_id
+    drink_name: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'drink',
+        key: 'name',
+      },
+    },
+  },
 ];
 
 const userDrinks = () => Drink.bulkCreate(userDrinksData);
