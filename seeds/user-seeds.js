087d8cb486316c7366d7261326c61fca46e24730
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-07 12:16:36 -0600

    Updated user-seeds.js

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,46 +1,26 @@
 // Perhaps user data here will store favorite drinks?
 // Create another file for user/created drinks, or store here too?
 
-// const { Drink } = require('../models');
-// // const { User } = require('../models');
+const { Users } = require('../models');
 
-const userDrinksData = [
+const userdata = [
   {
-    // user_id,
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-    // drink_id
-    drink_name: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'drink',
-        key: 'id',
-      },
-    },
-    // user_name,
-    user_id: {
-      type: DataTypes.STRING,
-      references: {
-        model: 'user',
-        key: 'name',
-      },
-    },
-    // drink_id
-    drink_name: {
-      type: DataTypes.STRING,
-      references: {
-        model: 'drink',
-        key: 'name',
-      },
-    },
+    username: 'alesmonde0',
+    email: 'nwestnedge0@cbc.ca',
+    password: 'password123'
   },
+  {
+    username: 'jwilloughway1',
+    email: 'rmebes1@sogou.com',
+    password: 'password123'
+  },
+  {
+    username: 'iboddam2',
+    email: 'cstoneman2@last.fm',
+    password: 'password123'
+  }
 ];
 
-const userDrinks = () => Drink.bulkCreate(userDrinksData);
+const seedUsers = () => Users.bulkCreate(userdata, {individualHooks: true});
 
-module.exports = userDrinks;
+module.exports = seedUsers;
