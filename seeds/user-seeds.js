734acef193b6ee0f4bf53fa32ae866bedf3c1600
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-07 12:17:45 -0600

    updated password to pw for userdata object property in user-seeds.js

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -7,17 +7,17 @@ const userdata = [
   {
     username: 'alesmonde0',
     email: 'nwestnedge0@cbc.ca',
-    password: 'password123'
+    pw: 'password123'
   },
   {
     username: 'jwilloughway1',
     email: 'rmebes1@sogou.com',
-    password: 'password123'
+    pw: 'password123'
   },
   {
     username: 'iboddam2',
     email: 'cstoneman2@last.fm',
-    password: 'password123'
+    pw: 'password123'
   }
 ];
 
