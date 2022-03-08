6bf7031aafc7b79b14be4be43cb5d1d5ed76ba1d
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-08 14:43:46 -0600

    removed last drink from Whiskey; error still occurs with npm run seed, but npm start works now!

--- controllers/api/drink-routes.js
+++ controllers/api/drink-routes.js
@@ -4,7 +4,7 @@ const withAuth = require('../../utils/auth');
 
 // Get all drinks
 router.get('/', (req, res) => {
-  console.log('======================');
+  console.log('==========Get All Drinks ============');
   Drink.findAll({
     attributes: [
       'id',
