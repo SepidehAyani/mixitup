6c314c3de4d9603d768a09cd78ee83772dd83c23
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-03-08 14:25:40 -0600

    added more users to user-seeds so now we have enough and there won't be a mis-match between user_id and beverages

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -15,6 +15,26 @@ const userdata = [
     username: 'iboddam2',
     email: 'cstoneman2@last.fm',
     pw: 'password123'
+  },
+  {
+    username: 'bonniepat',
+    email: 'bonniep@comcast.net',
+    pw: 'password123'
+  },
+  {
+    username: 'jillnelson',
+    email: 'jnelson@sogou.com',
+    pw: 'password123'
+  },
+  {
+    username: 'wrangler',
+    email: 'wesrang@sogou.com',
+    pw: 'password123'
+  },
+  {
+    username: 'zhigss',
+    email: 'zhiggs@biz.cnet',
+    pw: 'password123'
   }
 ];
 
